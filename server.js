b7b296ebbf0483d9f1b853d5449c0ba2ac833350
Kris Petersen <kris@neighborhuddle.com>
2017-01-20 15:24:42 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+//create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url); /* Redirect https traffic to http since that's all our free OpenWeather plan can handle */
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js', /* use script loader npm module because not build for webpack */
+    'script!foundation-sites/dist/js/foundation.min.js', /*different from the tutorial because of foundation version */
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery', /* look at Foundation video for explanation of whats happening here */
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      applicationStyles: 'app/styles/app.scss',
+      Navigation: 'app/components/Navigation.jsx'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'inline-source-map' /* allows us to view original code in the Chrome React dev tools */
+};
